a61af94d8fe7f987dbda80d52f38973200963af0
Your Name <you@example.com>
2016-09-23 11:25:14 +0300

    оптимизация кода и выполнение поставленной задачи

--- pprint_json.py
+++ pprint_json.py
@@ -5,29 +5,10 @@ def load_data(filepath):
     return json.load(open(filepath,'r'))
 
 def pretty_print_json(data):
-    i = 0
-    start_pos = 0
-    print(data[0])
-    print('')
-    print(' '*start_pos,'[')
-    start_pos = start_pos + 1
-    print(' '*start_pos,'{')
-
-    str_json = str(data[0])
-#    str_json.find('{',0,len(str_json))
-    print(str_json.find('{',0,len(str_json)))
-#    while i < len(data):
-
-
+    return json.dumps(data,sort_keys=True,ensure_ascii = False,indent=4)
+# sort_keys - ключи выводимого словаря будут отсортированы
+# ident - массивы и объекты в JSON будут выводиться с этим уровнем отступа
+# ensure_ascii - строки запишутся как есть (без экранирования)
 
 if __name__ == '__main__':
-    data = load_data('alc.json')
-    pretty_print_json(data)
-
-#        if data[i]["Cells"]["SeatsCount"] == data[max_seats]["Cells"]["SeatsCount"]:
-#            sp_max.append(data[i]["Cells"]["Name"])
-#        elif data[i]["Cells"]["SeatsCount"] > data[max_seats]["Cells"]["SeatsCount"]:
-#            max_seats = i; sp_max = []; sp_max.append(data[i]["Cells"]["Name"]) #нашли больший бар, очищаем список больших баров
-#        i = i + 1
-#    return sp_max
-
+    print(pretty_print_json(load_data('alc.json')))
