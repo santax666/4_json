61ef45ce2f0102165530485a6667b35c6a501475
Your Name1 <you1@example.com>
2016-09-23 10:00:17 +0300

    ...

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,32 @@ import json
 
 
 def load_data(filepath):
-    pass
-
+    return json.load(open(filepath,'r'))
 
 def pretty_print_json(data):
-    pass
+    i = 0
+    start_pos = 0
+    print(data[0])
+    print('')
+    print(' '*start_pos,'[')
+    start_pos = start_pos + 1
+    print(' '*start_pos,'{')
+
+    str_json = str(data[0])
+#    str_json.find('{',0,len(str_json))
+    print(str_json.find('{',0,len(str_json)))
+#    while i < len(data):
+
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('alc.json')
+    pretty_print_json(data)
+
+#        if data[i]["Cells"]["SeatsCount"] == data[max_seats]["Cells"]["SeatsCount"]:
+#            sp_max.append(data[i]["Cells"]["Name"])
+#        elif data[i]["Cells"]["SeatsCount"] > data[max_seats]["Cells"]["SeatsCount"]:
+#            max_seats = i; sp_max = []; sp_max.append(data[i]["Cells"]["Name"]) #нашли больший бар, очищаем список больших баров
+#        i = i + 1
+#    return sp_max
+
