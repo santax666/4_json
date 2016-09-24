0ab2e50557bc347323a5d67f4d816634298a83ad
Your Name <you@example.com>
2016-09-24 18:47:21 +0300

    Внесено указание файла через параметр

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 4_json
+Скрипт выводит содержимое JSON-файла в удобном ввиде (Pretty Print).
+Введите в консоли терминала: python3.5 pprint_json.py your_file.json
--- pprint_json.py
+++ pprint_json.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 
 def load_data(filepath):
@@ -7,9 +8,13 @@ def load_data(filepath):
 
 def pretty_print_json(data):
     return json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4)
-# sort_keys - ключи выводимого словаря будут отсортированы
-# ident - массивы и объекты в JSON будут выводиться с этим уровнем отступа
-# ensure_ascii - строки запишутся как есть (без экранирования)
 
 if __name__ == '__main__':
-    print(pretty_print_json(load_data('alc.json')))
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--help':
+            print("Скрипт выводит содержимое JSON-файла в удобном ввиде.")
+            print("Введите в терминале: python3.5 pprint_json.py youfile.json")
+        else:
+            print(pretty_print_json(load_data(sys.argv[1])))
+    else:
+        print("Не задан файл для вывода!")
